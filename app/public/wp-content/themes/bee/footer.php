947a7749cec601cb8ac21c6e56c0a2d6e44fd18a
Jordan Dobney <jorddobbo@gmail.com>
2020-07-29 13:51:23 +0100

    Mailchimp

--- app/public/wp-content/themes/bee/footer.php
+++ app/public/wp-content/themes/bee/footer.php
@@ -8,7 +8,7 @@
 
 			<!-- Begin Mailchimp Signup Form -->
 			<div class="footer__subscribe-form" id="mc_embed_signup">
-			<form action="https://dobbodesign.us10.list-manage.com/subscribe/post?u=ea9184824ee1bea88fb96f570&amp;id=b708dd2ff0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<form action="https://bee-space.us19.list-manage.com/subscribe/post?u=3750b10ca1080d2dbf08fc785&amp;id=5285c1cc17" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 			    <div id="mc_embed_signup_scroll">
 				
 			<input type="email" value="" name="EMAIL" class="required email footer__subscribe-input" id="mce-EMAIL" placeholder="Enter your email address" >
@@ -16,7 +16,7 @@
 					<div class="response" id="mce-error-response" style="display:none"></div>
 					<div class="response" id="mce-success-response" style="display:none"></div>
 				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-			    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ea9184824ee1bea88fb96f570_b708dd2ff0" tabindex="-1" value=""></div>
+			    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3750b10ca1080d2dbf08fc785_5285c1cc17" tabindex="-1" value=""></div>
 			    <div class="clear"><input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button footer__subscribe-button"></div>
 			    </div>
 			</form>
